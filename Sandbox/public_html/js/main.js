8b5d576c32113ddca6bf5512fead33e091d55142
Francis Crossen <francis.crossen@betbright.com>
2017-10-11 17:03:01 +0100

    Added JS code for this object scope

---
+++ Sandbox/public_html/js/main.js
@@ -0,0 +1,30 @@
+/*
+ * You Dont Know JavaScript sandbox
+ */
+(function iffy() {
+    // console.log('DOM ready');
+
+    function identify() {
+            return this.name.toUpperCase();
+    }
+
+    function speak() {
+            var greeting = "Hello, I'm " + identify.call( this );
+            console.log( greeting );
+    }
+
+    var me = {
+            name: "Kyle"
+    };
+
+    var you = {
+            name: "Reader"
+    };
+
+    identify.call( me ); // KYLE
+    identify.call( you ); // READER
+
+    speak.call( me ); // Hello, I'm KYLE
+    speak.call( you ); // Hello, I'm READER
+
+})();
