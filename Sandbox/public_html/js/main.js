94aeaba418cf89a1b9d158c92fd312beb9f538df
Francis Crossen <francis.crossen@betbright.com>
2017-10-13 11:29:38 +0100

    Commented out curry/bind demo

--- Sandbox/public_html/js/main.js
+++ Sandbox/public_html/js/main.js
@@ -195,13 +195,17 @@ new foo(); // Object
 foo(); // global
 */
 
+/*
+ * Currying with bind...
+ */
 function foo(a,b) {
 	console.log( "a:" + a + ", b:" + b );
 }
 
 // spreading out array as parameters
-foo.apply( null, [2, 3] ); // a:2, b:3
+// foo.apply( null, [2, 3] ); // a:2, b:3
 
 // currying with `bind(..)`
+
 var bar = foo.bind( null, 2 );
 bar( 3 ); // a:2, b:3
